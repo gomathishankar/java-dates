9abb0aa37e2c4093f7efc147167207260a140efd
gomathishankar <gomathishankarbe@gmail.com>
2022-01-11 19:35:36 +0530

    My Initial Commit

---
+++ src/main/java/com/serenitydojo/DateAssertions.java
@@ -0,0 +1,26 @@
+package com.serenitydojo;
+
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.AbstractBigDecimalAssert;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class DateAssertions extends AbstractAssert<DateAssertions, LocalDate> {
+
+    public DateAssertions(LocalDate localDate) {
+        super(localDate, DateAssertions.class);
+    }
+
+
+    public static DateAssertions assertThat(LocalDate actual) {
+        return new DateAssertions(actual);
+    }
+
+    public DateAssertions isEqualTo(int expectedDay, Month expectedMonth, int expectedYear) {
+        if (actual.getYear() != expectedYear || actual.getMonth() != expectedMonth || actual.getDayOfMonth() != expectedDay) {
+            failWithMessage("Expected a date of %s %s %s but was %s", expectedDay, expectedMonth, expectedYear,actual);
+        }
+        return this;
+    }
+}
--- src/test/java/com/serenitydojo/WhenWorkingWithDates.java
+++ src/test/java/com/serenitydojo/WhenWorkingWithDates.java
@@ -1,8 +1,19 @@
 package com.serenitydojo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("When working with dates in Java 8")
 class WhenWorkingWithDates {
@@ -14,16 +25,46 @@ class WhenWorkingWithDates {
         @Test
         @DisplayName("We can create a date by specifying the year, month and day")
         void creatingASimpleDate() {
+            LocalDate jan26 = LocalDate.of(2022, Month.JANUARY, 26);
+
+            DateAssertions.assertThat(jan26).isEqualTo(2022, Month.JANUARY, 26);
+
+            assertThat(jan26.getYear()).isEqualTo(2022);
+            assertThat(jan26.getMonth()).isEqualTo(Month.JANUARY);
+            assertThat(jan26.getDayOfMonth()).isEqualTo(26);
         }
 
         @Test
         @DisplayName("We can create a date by using a string")
         void creatingASimpleDateFromAString() {
+            LocalDate jan26 = LocalDate.parse("2022-01-26");
+
+
+            assertThat(jan26.getYear()).isEqualTo(2022);
+            assertThat(jan26.getMonth()).isEqualTo(Month.JANUARY);
+            assertThat(jan26.getDayOfMonth()).isEqualTo(26);
         }
 
         @Test
         @DisplayName("We can create a date by using a formatted string")
         void createADateFromAFormattedString() {
+            LocalDate jan26 = LocalDate.parse("2022/26/01", DateTimeFormatter.ofPattern("yyyy/dd/MM"));
+
+
+            assertThat(jan26.getYear()).isEqualTo(2022);
+            assertThat(jan26.getMonth()).isEqualTo(Month.JANUARY);
+            assertThat(jan26.getDayOfMonth()).isEqualTo(26);
+        }
+
+        @Test
+        @DisplayName("We can create a date by using a different formatted string")
+        void createADateFromADifferentFormattedString() {
+            LocalDate jan26 = LocalDate.parse("Jan 26, 2022", DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+
+
+            assertThat(jan26.getYear()).isEqualTo(2022);
+            assertThat(jan26.getMonth()).isEqualTo(Month.JANUARY);
+            assertThat(jan26.getDayOfMonth()).isEqualTo(26);
         }
     }
 
@@ -33,21 +74,40 @@ class WhenWorkingWithDates {
         @Test
         @DisplayName("We can create a date for the following day")
         void findTheNextDay() {
+            LocalDate jan26 = LocalDate.parse("2022-01-26");
+            //  jan26.plus(1, ChronoUnit.DAYS);
+            LocalDate tomorrow = jan26.plusDays(1);
+            assertThat(tomorrow.getDayOfMonth()).isEqualTo(27);
         }
 
         @Test
         @DisplayName("We can create a date for the following month")
         void findTheNextMonth() {
+            LocalDate jan26 = LocalDate.parse("2022-01-26");
+            LocalDate nextMonth = jan26.plusMonths(1);
+
+            DateAssertions.assertThat(nextMonth).isEqualTo(26, Month.FEBRUARY, 2022);
         }
 
         @Test
         @DisplayName("We can find a specific day of the week")
         void findTheNextMonday() {
+            LocalDate jan26 = LocalDate.parse("2022-01-26");
+            LocalDate followingMonday = findFollowingWeekDay(jan26, DayOfWeek.MONDAY);
+
+            DateAssertions.assertThat(followingMonday).isEqualTo(31, Month.JANUARY, 2022);
+
         }
 
         @Test
         @DisplayName("We can find a specific day of the week using temporal adjusters")
         void usingTemporalAdjusters() {
+
+            LocalDate jan26 = LocalDate.parse("2022-01-26");
+            LocalDate followingMonday = jan26.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+
+            DateAssertions.assertThat(followingMonday).isEqualTo(31, Month.JANUARY, 2022);
+
         }
 
         @Test
@@ -60,6 +120,15 @@ class WhenWorkingWithDates {
         void usingOtherTemporalAdjusters() {
         }
 
+    }
+
+    private LocalDate findFollowingWeekDay(LocalDate startDate, DayOfWeek expectedDayOfWeek) {
+        LocalDate followingDay = startDate;
+        while (followingDay.getDayOfWeek() != expectedDayOfWeek) {
+            followingDay = followingDay.plusDays(1);
+
+        }
+        return followingDay;
     }
 
     @Nested
